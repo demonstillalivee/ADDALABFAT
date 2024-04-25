75aa30076a75f67ca72bba38cb349a816c1bcdeb
VISHAL JAIN <74926335+demonstillalivee@users.noreply.github.com>
2024-04-25 12:30:12 +0530

    Add files via upload

---
+++ labfat.java
@@ -0,0 +1,8 @@
+public class labfat {
+        public static void main(String[] args) {
+            System.out.println("20MIC0161\n VISHAL JAIN");
+            System.out.println("ADDA LABFAT OUTPUT");
+            System.out.println("date: 25th April 2024");
+        }
+    
+}
